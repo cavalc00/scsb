da5a6930a806a083687bfd771e5f8bb11e1e7468
cavalc00 <vinicius.caval00@gmail.com>
2024-05-03 14:40:18 -0300

    Added spring boot

---
+++ src/main/java/br/com/scsb/SistemaDeCriptografiaSimetricaDeBlocosApplication.java
@@ -0,0 +1,13 @@
+package br.com.scsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeCriptografiaSimetricaDeBlocosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeCriptografiaSimetricaDeBlocosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/scsb/SistemaDeCriptografiaSimetricaDeBlocosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.scsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeCriptografiaSimetricaDeBlocosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
